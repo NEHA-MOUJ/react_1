73dedb31cfb73f17bf4dc64cd486eae42e131759
NEHA-MOUJ <nehamouj815@gmail.com>
2020-06-07 17:52:44 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      hello to every one
+      </div>
+      
+  );
+}
+
+export default App;
